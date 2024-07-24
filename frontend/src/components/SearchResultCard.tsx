9c1323132e356901ad6af3c5b974a43a13360a56
cryptomathematician <bosman006@st.ug.edu.gh>
2024-07-24 15:34:22 +0000

    Changed the aspect ratio of an image

--- frontend/src/components/SearchResultCard.tsx
+++ frontend/src/components/SearchResultCard.tsx
@@ -13,7 +13,7 @@ const SearchResultCard = ({ artisan }: Props) => {
       to={`/detail/${artisan._id}`}
       className="grid lg:grid-cols-[2fr_3fr] gap-5 group"
     >
-      <AspectRatio ratio={16 / 6}>
+      <AspectRatio ratio={5/ 3}>
         <img
           src={artisan.imageUrl}
           className="rounded-md w-full h-full object-cover"
--- frontend/src/pages/OrderStatusPage.tsx
+++ frontend/src/pages/OrderStatusPage.tsx
@@ -21,7 +21,7 @@ const OrderStatusPage = () => {
           <OrderStatusHeader order={order} />
           <div className="grid gap-10 md:grid-cols-2">
             <OrderStatusDetail order={order} />
-            <AspectRatio ratio={16 / 5}>
+            <AspectRatio ratio={3/ 2}>
               <img
                 src={order.artisan.imageUrl}
                 className="rounded-md object-cover h-full w-full"
